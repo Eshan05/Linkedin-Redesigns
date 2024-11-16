e38101ae67cd092cfa5c3fbbbdd682cd135f67a4
Eshan05 <nahareshan05@gmail.com>
2024-11-16 13:51:46 +0530

    feat(angular): Add a dropdown to navigation

--- angular/src/app/navbar/navbar.component.ts
+++ angular/src/app/navbar/navbar.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ThemeService } from '../services/theme.service';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 import { MainIconsComponent } from "./main-icons/main-icons.component";
 import { AvatarComponent } from "../avatar/avatar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ThemeToggleComponent, MainIconsComponent, AvatarComponent],
+  imports: [ThemeToggleComponent, MainIconsComponent, AvatarComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   constructor(public themeService: ThemeService) { }
+  dropdownVisible = false;
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.profile-icon')) {
+      this.dropdownVisible = false;
+    }
+  }
 }
