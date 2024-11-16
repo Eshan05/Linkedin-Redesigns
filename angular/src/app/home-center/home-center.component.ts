363f18679ce43f5261333c3026a12e2779893ec3
Eshan05 <nahareshan05@gmail.com>
2024-11-16 18:06:31 +0530

    feat(angular): More or less done with post design

--- angular/src/app/home-center/home-center.component.ts
+++ angular/src/app/home-center/home-center.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AvatarComponent } from "../avatar/avatar.component";
 
 @Component({
   selector: 'app-home-center',
   standalone: true,
-  imports: [],
+  imports: [AvatarComponent],
   templateUrl: './home-center.component.html',
   styleUrl: './home-center.component.css'
 })
